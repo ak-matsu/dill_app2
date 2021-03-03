b2bc49138f8c0dd779cc590642fe7fac5a9aeae6
ak-matsu <sandbox.matsu@gmail.com>
2021-03-03 23:33:35 +0900

    posts/newのルーティング設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   #indexのアクションを定義
   def index
+    # 複数形、全てのデータを扱いたい場合@posts
+    @posts = Post.all
+  end
+
+  def new
     
-  end  
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   #ルーティングの設定
+  #indexへアクセスする際はpostsのみでposts#index
   get 'posts', to: 'posts#index'
+  #posts/newはposts#newである
+  get 'posts/new', to: 'posts#new'
 end
